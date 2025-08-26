6e24df1caee392a9d2567c938108067573569c96
naxor <naxordeve@gmail.com>
2025-08-26 17:45:04 +0200

    naxor

--- session.js
+++ session.js
@@ -2,24 +2,19 @@ import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
 
-async function SessionCode(session, fd) {
-    try {
+export async function SessionCode(session, fd) {
+  try {
     if (!session) throw new Error("Invalid SESSION_ID format");
     const x = session.includes("~") ? session.split("~")[1] : session;
     const ctx = `https://pastebin.com/raw/${x}`;
-    const con = {method: 'get',url: ctx};
-    const res = await axios(con);   
-    if (!res.data) throw new Error("Session data missing");   
-    if (!fs.existsSync(fd)) fs.mkdirSync(fd, { recursive: true });   
+    const res = await axios.get(ctx);
+    if (!res.data) throw new Error("Session data missing");
+    if (!fs.existsSync(fd)) fs.mkdirSync(fd, { recursive: true });
     const n = path.join(fd, "creds.json");
-    const conn = typeof res.data === "string" 
-    ? res.data 
-    : JSON.stringify(res.data);
+    const conn = typeof res.data === "string" ? res.data : JSON.stringify(res.data);
     fs.writeFileSync(n, conn);
     console.log("✅ connected");
-    } catch (error) {
+  } catch (error) {
     console.error(error.message);
-    }
+  }
 }
-
-module.exports = { SessionCode };
